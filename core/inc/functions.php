3ee2d5d1eb46d9ab28b8c3c76dd006af4e9c0dab
adamaoc <adamaoc@gmail.com>
2013-09-26 07:03:37 -0500

    updated dynamic title function

--- core/inc/functions.php
+++ core/inc/functions.php
@@ -19,13 +19,9 @@
 	function dynamicTitle($onpage) {
 		if (isset($onpage)) {
 			if ($onpage == 'index') {
-				return "Online Shopping for Seasonal and Holiday Decorations.";
-			} elseif ($onpage == 'store') {
-				return "Our online store has great options for Seasonal and Holiday Decorations and more!";
-			} elseif ($onpage == 'about') {
-				return "Learn more about our comapany and how we can help you save money during the holidays.";
-			} elseif ($onpage == 'contact') {
-				return "Contact with any questions you might have.";
+				return "Websites - SEO - Social Media";
+			} else {
+				return "";
 			}
 		}
 	}
