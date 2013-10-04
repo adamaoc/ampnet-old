160f92a8558720231429cf6f780c4fd5018a9a7c
adamaoc <adamaoc@gmail.com>
2013-10-04 16:27:21 -0500

    added apple icons and favicon

--- sharedviews/header.php
+++ sharedviews/header.php
@@ -8,13 +8,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Online shopping for all your seasonal or holiday decorations. Great for the home or office and sold at amazing prices!">
     <meta name="author" content="ampnetmedia.com">
-    <link rel="shortcut icon" href="/img/favicon.png">
+    <link rel="shortcut icon" href="/img/favicon.ico">
     <meta name="google-site-verification" content="fSdVe-xL70WGQ4EdrP7dZemxzq1oYbc_GI48aFY1_0w" />
     <meta name="msvalidate.01" content="B038142E19DDC9427F7BB86F8022EE9C" />
     
     <title>ampnet(media) - <?php echo dynamicTitle($onpage); ?></title>
     
-    <?php // make a getTitle() function that dynamically displays title name for diff pages. ?>
+    <link rel="apple-touch-icon" href="/img/touch-icon-iphone.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="/img/touch-icon-ipad.png" />
+    <link rel="apple-touch-icon" sizes="120x120" href="/img/touch-icon-iphone-retina.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="/img/touch-icon-ipad-retina.png" />
     
     <link href="/css/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
     <!--[if IE]>
